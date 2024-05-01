b86ce64e5f237fb33e43a005324a02ff3f98b142
Antonio Lindo <antoniolvitall@gmail.com>
2024-05-01 19:15:06 -0300

    imagem do perfil

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from 'react'
 import getProfile from '../../services/profileAPI'
 
 function Profile() {
-  const [profile, setProfile] = useState<{ name: string } | null>(null);
+  const [profile, setProfile] = useState<{
+    avatar_url: string | undefined; name: string 
+} | null>(null);
 
   useEffect(() => {
     getProfile().then(data => setProfile(data));
@@ -10,6 +12,7 @@ function Profile() {
 
   return (
     <div>
+      {profile && <img src={profile.avatar_url} alt="" />}
       <h1>
         {profile ? `Hello, ${profile.name}!` : 'Loading...'}
       </h1>
