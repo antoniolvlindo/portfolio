ee4527fab144be7aac95548d6d48d214a5646cd4
Antonio Lindo <antoniolvitall@gmail.com>
2024-05-02 12:23:38 -0300

    melhorando codigo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import './App.css'
-import Profile from './components/profile/Profile';
+import { useEffect, useState } from 'react'
+import getProfile from './services/profileAPI'
+import Profile from './components/profile/Profile'
 
 function App() {
-  return(
-    <Profile />
-  );
+  const [profile, setProfile] = useState<{ avatar_url: string; name: string } | null>(null);
+
+  useEffect(() => {
+    getProfile().then(data => setProfile(data));
+  }, []);
+
+  return (
+    <div>
+      <Profile profile={profile} />
+    </div>
+  )
 }
 
 export default App
--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,23 +1,23 @@
-import React, { useEffect, useState } from 'react'
-import getProfile from '../../services/profileAPI'
+import React from "react";
 
-function Profile() {
-  const [profile, setProfile] = useState<{
-    avatar_url: string | undefined; name: string 
-} | null>(null);
+interface ProfileProps {
+  profile: {
+    avatar_url: string;
+    name: string;
+  } | null;
+}
 
-  useEffect(() => {
-    getProfile().then(data => setProfile(data));
-  }, []);
+function Profile({ profile }: ProfileProps) {
+  if (!profile) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div>
-      {profile && <img src={profile.avatar_url} alt="" />}
-      <h1>
-        {profile ? `Hello, ${profile.name}!` : 'Loading...'}
-      </h1>
+      <img src={profile.avatar_url} alt="" />
+      <h1>Hello, {profile.name}!</h1>
     </div>
-  )
+  );
 }
 
 export default Profile
