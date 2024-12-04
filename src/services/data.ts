37f81678b5e2f1c7aecccd596796415803924e77
Antonio Lindo <antoniolvitall@gmail.com>
2024-12-04 18:42:48 -0300

    feat: criação de um data para colocar as informações que serão mostradas

---
+++ src/services/data.ts
@@ -0,0 +1,56 @@
+const projects = [
+  {
+    title: "Recipes App",
+    description: "Projeto desenvolvido para ajudar os usuários a encontrar e gerenciar receitas de comidas e bebidas. O aplicativo permite pesquisar receitas, visualizar detalhes, marcar receitas como favoritas, e acompanhar o progresso de receitas em andamento.",
+    github: "https://github.com/antoniolvlindo/project-recipes-app",
+    link: "https://project-recipes-app-sage.vercel.app/",
+    technologies: [
+      "React",
+      "TypeScript",
+      "Context API",
+      "Axios",
+      "Bootstrap",
+      "CSS Modules",
+      "ESLint",
+      "Vitest"
+    ]
+  },
+  {
+    title: "Trybe Futebol Clube",
+    description: "Projeto full-stack que simula a gestão de partidas de um campeonato de futebol.",
+    github: "https://github.com/antoniolvlindo/trybe_futebol_clube",
+    technologies: [
+      "Node.js",
+      "ExpressJS",
+      "TypeScript",
+      "Sequelize",
+      "MySQL",
+      "JWT",
+      "Mocha",
+      "Chai",
+      "Sinon",
+      "React",
+      "Docker"
+    ]
+  }
+];
+
+const educations = [
+  {
+    title: "Desenvolvimento Web Full Stack - Trybe",
+    period: "Conclusão em Dezembro de 2024",
+    description: "1.500 horas de aula com foco em: Introdução ao desenvolvimento de software, front-end, back-end, ciência da computação, engenharia de software, metodologias ágeis e habilidades comportamentais."
+  },
+  {
+    title: "Graduação em Ciência da Computação - Estácio",
+    period: "Em andamento",
+    description: ""
+  },
+  {
+    title: "Escola Técnica Electra - Técnico em Eletrotécnica",
+    period: "Conclusão em Dezembro de 2009",
+    description: ""
+  }
+];
+
+export { projects, educations };
