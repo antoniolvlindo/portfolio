9061e30faa616afda6f432840da46423240e740b
Antonio Lindo <antoniolvitall@gmail.com>
2024-12-09 17:34:32 -0300

    feat: :sparkles: Area de experiencias

--- src/services/data.ts
+++ src/services/data.ts
@@ -53,4 +53,31 @@ const educations = [
   }
 ];
 
-export { projects, educations };
+const experiences = [
+  {
+    company: "Empresa Pública de Saúde do Rio de Janeiro S/A - RioSaúde",
+    position: "Assistente Administrativo",
+    period: "03/2020 até 01/2024",
+    description: "Lotado no Centro Emergencial Regional da Barra da Tijuca - CER Barra - Responsável pela digitação e envio do Boletim de Produção Ambulatorial (BPA), CNES, extração de relatórios e acompanhamento das metas da unidade, acompanhamento de serviços de manutenção estrutural e de TI, inserção de notas fiscais, acompanhamento de contratos, controle de almoxarifado, suporte à gerência e outros setores e resolução de problemas mais complexos que envolvam conhecimento tecnológico."
+  },
+  {
+    company: "Banco do Brasil Tecnologia e Serviços S/A",
+    position: "Estágio em Administração",
+    period: "06/2019 até 03/2020",
+    description: "Recursos humanos com ênfase em treinamento interno - Responsável pelo acompanhamento das horas de treinamento, organização de cursos internos realizados pela própria empresa. Referência no setor para confecção de planilhas mais complexas."
+  },
+  {
+    company: "Certpremium Certificadora Digital Ltda",
+    position: "Agente de Registro",
+    period: "10/2017 até 07/2018",
+    description: "Atendimento a clientes internos e externos na realização de certificados digitais, controles administrativos e financeiros do escritório e prospecção de clientes."
+  },
+  {
+    company: "Site-Tec Engenharia de Serviços Ltda",
+    position: "Comprador",
+    period: "06/2012 até 01/2017",
+    description: "Controle do estoque de materiais, compra de insumos para"
+  }
+];
+
+export { projects, educations, experiences};
