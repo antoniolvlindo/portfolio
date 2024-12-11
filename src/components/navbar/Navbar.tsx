a7a3caf05020e236bc472e5f299695dccb3e9795
Antonio Lindo <antoniolvitall@gmail.com>
2024-12-10 22:29:48 -0300

    feat: :sparkles: link para o curriculo

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -4,7 +4,14 @@ function Navbar() {
   return (
     <nav className="flex items-center justify-between mb-40">
       <img src={allogo} alt="Logo" width="60px"/>
-      <button className="px-7 py-3 md:px-9 md:py-4 bg-white font-medium md:font-semibold text-gray-700 text-md rounded-md hover:bg-gray-700 hover:text-white transition ease-linear duration-500">Get my CV</button>
+      <a
+        href="https://drive.google.com/file/d/1AXJgpU6idcu2UIil-l5hXMzu-q24lClh/view?usp=sharing"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="px-7 py-3 md:px-9 md:py-4 bg-white font-medium md:font-semibold text-gray-700 text-md rounded-md hover:bg-gray-700 hover:text-white transition ease-linear duration-500"
+      >
+        Get my CV
+      </a>
     </nav>
   );
 }
