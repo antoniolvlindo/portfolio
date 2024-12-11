5ca665404290b9e17cf73bf3ef668161f8817f0b
Antonio Lindo <antoniolvitall@gmail.com>
2024-12-10 22:06:08 -0300

    refactor: :art: alteracao na secao de experiencias

--- src/components/experience/experience.tsx
+++ src/components/experience/experience.tsx
@@ -6,31 +6,15 @@ function ExperienceSection() {
     <section className="py-10 md:py-16">
       <div className="container max-w-screen-xl mx-auto px-4">
         <h1 className="font-medium text-gray-700 text-3xl md:text-4xl mb-5">Experiências</h1>
-        <div className="flex flex-col lg:flex-row justify-between">
-          <div className="space-y-8 md:space-y-16 mb-16 md:mb-0">
-            <h6 className="font-medium text-gray-400 text-base uppercase">Empresa</h6>
-            {experiences.map((experience, index) => (
-              <p key={index} className="font-semibold text-gray-600 text-base">
-                {experience.company}
-              </p>
-            ))}
-          </div>
-          <div className="space-y-8 md:space-y-16 mb-16 md:mb-0">
-            <h6 className="font-medium text-gray-400 text-base uppercase">Cargo</h6>
-            {experiences.map((experience, index) => (
-              <p key={index} className="font-normal text-gray-400 text-base">
-                {experience.position}
-              </p>
-            ))}
-          </div>
-          <div className="space-y-8 md:space-y-16">
-            <h6 className="font-medium text-gray-400 text-base uppercase">Ano</h6>
-            {experiences.map((experience, index) => (
-              <p key={index} className="font-normal text-gray-400 text-base">
-                {experience.period}
-              </p>
-            ))}
-          </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {experiences.map((experience, index) => (
+            <div key={index} className="bg-gray-50 px-8 py-10 rounded-md">
+              <h4 className="font-medium text-gray-700 text-lg mb-4">{experience.company}</h4>
+              <p className="font-normal text-gray-500 text-md mb-2">{experience.position}</p>
+              <p className="font-normal text-gray-500 text-md mb-2">{experience.period}</p>
+              <p className="font-normal text-gray-500 text-md">{experience.description}</p>
+            </div>
+          ))}
         </div>
       </div>
     </section>
